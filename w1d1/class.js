b6f7e0c453bfff59fe75cac9ea106520f15963e5
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-04-19 16:58:20 -0400

    update

--- w1d1/class.js
+++ w1d1/class.js
@@ -1,1 +1,11 @@
 "use strict";
+Algorithm removeDuplicates(L)
+    Input array L
+    Output new List, M containing all distinct Items 
+    
+    M new list
+    for i<-0 to L.size -1 do 
+        if not M.contains(l[i]) then 
+            M.add(L[i])
+
+    return M
