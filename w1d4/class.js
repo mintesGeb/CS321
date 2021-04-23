027532e159205060ca7693d0ea2d2c10a92d5be7
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-04-23 11:13:33 -0400

    update

--- w1d4/class.js
+++ w1d4/class.js
@@ -41,13 +41,58 @@ function sum(arr) {
   return arr[0] + sum(arr.slice(1));
 }
 
-function sumFast(arr, index) {
-  if (index === arr.length - 1) {
-    return arr[index];
-  }
-  return arr[index] + sumFast(arr, index + 1);
+function sum(arr) {
+  return sumHelper(arr, 0);
 }
-console.log(sumFast([1, 2, 3, 4], 0));
+function sumHelper(arr, index) {
+  if (index === arr.length) {
+    return 0;
+  }
+  return arr[index] + sumHelper(arr, index + 1);
+}
+console.log(sum([1, 2, 3, 4]));
+
+// Algorithm sum(arr)
+//  input: array to add elements from
+//  output: sum of the elements of the array
+
+//  return sumHelper(arr, 0)
+
+// Algorithm sumHelper(arr, index)
+//   input: array and index the initial index to start sum
+//   output: sum of each element of the array
+
+//   if index = arr.length then return 0
+//   return arr[index] +sumHelper(arr , index+1)
+
+function max(arr) {
+  return maxhelper(arr, 0);
+}
+function maxhelper(arr, index) {
+  if (index === arr.length) {
+    return 0;
+  }
+
+  let toCheck = maxhelper(arr, index + 1);
+  if (arr[index] > toCheck) return arr[index];
+  else return toCheck;
+  // return arr[index] > toCheck ? arr[index] : toCheck;
+}
+console.log(max([1, 2, 3, 8, 4, 9]));
+
+// Algorithm findMax(arr)
+//   input: array to check max value FormData
+//   output: max value
+
+//   return maxhelper(arr,0);
+
+// Algorithm findMaxhelper(arr, index)
+//   input: array and index to see which has maximum value
+//   output: the maximum value in the array
+//  toCheck <- findMaxhelper(arr, index+1)
+//   if index =  arr.length then return 0;
+//   if arr[index]  > toCheck then return arr[index]
+//   else return toCheck
 
 function binarySearh(arr, n, lo, hi = arr.length - 1) {
   let mid = Math.floor(hi - lo + 1 / 2);
