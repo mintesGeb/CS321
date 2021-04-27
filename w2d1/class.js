0127e6ab36f451663e18a6f8295d76ac7c70e6e6
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-04-27 15:37:29 -0400

    update

--- w2d1/class.js
+++ w2d1/class.js
@@ -26,34 +26,34 @@ let arr1 = [1, 3, 4, 6, 8];
 let arr2 = [2, 5, 7];
 // console.log(merge(arr1, arr2));
 
-// function quickSort(arr) {
-//   if (arr.length === 1 || arr.length === 0) return arr;
-//   let r = pickPivot(arr);
-//   let { l, e, g } = partition(arr, r);
-//   l = quickSort(l);
-//   g = quickSort(g);
-//   return merge(merge(l, e), g);
-// }
+function quickSort(arr) {
+  if (arr.length === 1 || arr.length === 0) return arr;
+  let r = pickPivot(arr);
+  let { l, e, g } = partition(arr, r);
+  l = quickSort(l);
+  g = quickSort(g);
+  return merge(merge(l, e), g);
+}
 
-// function pickPivot(arr) {
-//   let r = Math.floor(Math.random() * arr.length);
+function pickPivot(arr) {
+  let r = Math.floor(Math.random() * arr.length);
 
-//   return r;
-// }
-// console.log(pickPivot([2, 4, 6, 5, 78, 9, 4, 5]));
+  return r;
+}
+console.log(pickPivot([2, 4, 6, 5, 78, 9, 4, 5]));
 
-// function partition(arr, r) {
-//   let l = [];
-//   let g = [];
-//   let e = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] > arr[r]) g.push(arr[i]);
-//     else if (arr[i] < arr[r]) l.push(arr[i]);
-//     else e.push(arr[i]);
-//   }
+function partition(arr, r) {
+  let l = [];
+  let g = [];
+  let e = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[r]) g.push(arr[i]);
+    else if (arr[i] < arr[r]) l.push(arr[i]);
+    else e.push(arr[i]);
+  }
 
-//   return { l, e, g };
-// }
+  return { l, e, g };
+}
 // console.log(quickSort([2, 4, 6, 5, 78, 9, 4, 5]));
 
 function swap(arr, i, j) {
@@ -61,54 +61,54 @@ function swap(arr, i, j) {
   arr[i] = arr[j];
   arr[j] = temp;
 }
-function pivotPicker(arr, lo, hi) {
-  return Math.floor(Math.random() * (hi - lo + 1));
-}
-
-function inPlaceQuickSort(arr, l = 0, r = arr.length - 1) {
-  if (l >= r) return arr;
-  let k = pivotPicker(arr, l, r);
-  //   console.log(k);
-
-  swap(arr, r, k);
-  let p = arr[r];
-  let i = inPlacePartition(arr, l, r, p);
-  let left = inPlaceQuickSort(arr, l, i - 1);
-  let right = inPlaceQuickSort(arr, i + 1, r);
-  return left;
-}
-
-function inPlacePartition(arr, lo, hi, pi) {
-  console.log(lo, hi, arr);
-
-  let i = lo;
-  let j = hi - 1;
-  let iStuck;
-  while (i <= j) {
-    if (arr[i] < pi) {
-      i++;
-    } else {
-      iStuck = i;
-      if (arr[j] > pi) {
-        j--;
-      } else {
-        swap(arr, iStuck, j);
-        i++;
-        j--;
-      }
-    }
-  }
-  swap(arr, i, hi);
-  console.log(arr);
-  console.log("----------");
-  return i;
-}
-// function quickSortHelper(arr,p,l=0,r=arr.length-1){
-//     swap(arr,p,r);
-//     while (l<=r ){
-//         if (arr[i]>)
-//     }
+// function pivotPicker(arr, lo, hi) {
+//   return Math.floor(Math.random() * (hi - lo + 1));
 // }
-console.log(inPlaceQuickSort([2, 4, 6, 5, 78, 9, 4, 5]));
-// 2, 4, 5,  4, 5, 78, 6, 9
-//           j  i
+
+// function inPlaceQuickSort(arr, l = 0, r = arr.length - 1) {
+//   if (l >= r) return arr;
+//   let k = pivotPicker(arr, l, r);
+//   //   console.log(k);
+
+//   swap(arr, r, k);
+//   let p = arr[r];
+//   let i = inPlacePartition(arr, l, r, p);
+//   let left = inPlaceQuickSort(arr, l, i - 1);
+//   let right = inPlaceQuickSort(arr, i + 1, r);
+//   return left;
+// }
+
+// function inPlacePartition(arr, lo, hi, pi) {
+//   console.log(lo, hi, arr);
+
+//   let i = lo;
+//   let j = hi - 1;
+//   let iStuck;
+//   while (i <= j) {
+//     if (arr[i] < pi) {
+//       i++;
+//     } else {
+//       iStuck = i;
+//       if (arr[j] > pi) {
+//         j--;
+//       } else {
+//         swap(arr, iStuck, j);
+//         i++;
+//         j--;
+//       }
+//     }
+//   }
+//   swap(arr, i, hi);
+//   console.log(arr);
+//   console.log("----------");
+//   return i;
+// }
+// // function quickSortHelper(arr,p,l=0,r=arr.length-1){
+// //     swap(arr,p,r);
+// //     while (l<=r ){
+// //         if (arr[i]>)
+// //     }
+// // }
+// console.log(inPlaceQuickSort([2, 4, 6, 5, 78, 9, 4, 5]));
+// // 2, 4, 5,  4, 5, 78, 6, 9
+// //           j  i
