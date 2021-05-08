bac9d7f9bf7c5655b7de51aef4eb3df742255812
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-05-08 07:02:04 -0400

    up

--- class.js
+++ class.js
@@ -13,33 +13,123 @@ class BinarySearchTree {
   insert(val) {
     let newNode = new Node(val);
     let pos;
-    if (!this.root) {
+    if (this.root === null) {
       this.root = newNode;
+      return this;
     } else {
       let current = this.root;
-      let prev = current;
-      while (current) {
-        while (current.value >= newNode.value) {
-          prev = current;
-          current = current.left;
-          pos = "left";
-          if (!current) break;
-        }
-        while (current.value <= newNode.value) {
-          prev = current;
-          current = current.right;
-          pos = "right";
-          if (!current) break;
+      while (true) {
+        if (current.value === val) return undefined;
+        if (current.value > val) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
         }
       }
-      if (pos == "left") {
-        prev.left = newNode;
-      } else prev.right = newNode;
     }
-    return this;
+  }
+  find(val) {
+    if (!this.root) return false;
+
+    let current = this.root;
+    let found = false;
+
+    while (current && !found) {
+      if (current.value > val) {
+        current = current.left;
+      } else if (current.value < val) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    return current;
+  }
+  contains(val) {
+    if (!this.root) return false;
+
+    let current = this.root;
+    let found = false;
+
+    while (current && !found) {
+      if (current.value > val) {
+        current = current.left;
+      } else if (current.value < val) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+  BFS() {
+    let node = this.root,
+      data = [],
+      queue = [];
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+
+  DFS_pre() {
+    let data = [];
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
+  DFS_post() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
+  DFS_inOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
   }
 }
 let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(3);
 tree.insert(8);
-tree.insert(7);
+tree.insert(15);
+tree.insert(20);
+tree.insert(12);
+console.log(tree.find(12));
+console.log(tree.insert(11));
 console.log(tree);
+// console.log(tree.BFS());
+// console.log(tree.DFS_pre());
+// console.log(tree.DFS_post());
+console.log(tree.DFS_inOrder());
---
+++ w3d4/MinHeap.js
@@ -0,0 +1,115 @@
+class HNode {
+    constructor(elem, index) {
+        this._elem = elem;
+        this._index = index;
+    }
+    element() {
+        return this._elem;
+    }
+}
+class MinHeap {
+    constructor(comparator) {
+        this._size = 0;
+        this._heap = new Array();
+        this._compare = comparator;
+        this._compCount = 0;
+        this._swapCount = 0;
+    }
+    isEmpty() {
+        return this._size == 0;
+    }
+    size() {
+        return this._size;
+    }
+    _less(pos1, pos2) {
+        this._compCount++;
+        return this._compare(pos1.element(), pos2.element()) < 0;
+    }
+    _greater(pos1, pos2) {
+        this._compCount++;
+        return this._compare(pos1.element(), pos2.element()) > 0;
+    }
+    _swapItems(p, i) {
+        let temp = this._heap[p];
+        this._heap[p] = this._heap[i];
+        this._heap[p]._index = p;
+        this._heap[i] = temp;
+        this._heap[i]._index = i;
+        this._swapCount++;
+    }
+    _upHeap(i) {
+        let parent = Math.floor(i/2);
+        while (parent >= 1 && this._greater(this._heap[parent], this._heap[i])) {
+            this._swapItems(parent, i);
+            i = parent;
+            parent = Math.floor(i/2);
+        }
+    }
+    _indexOfMinKey(r) {
+        let smallest = r;
+        let left = r * 2;
+        let right = r * 2 + 1;
+        if (left <= this._size) {
+            if (this._less(this._heap[left], this._heap[smallest])) {
+                smallest = left;
+            }
+        }
+        if (right <= this._size) {
+            if (this._less(this._heap[right], this._heap[smallest])) {
+                smallest = right; 
+            }
+        }
+        return smallest;
+    }
+    _downHeap(i) {
+        let hasSwap = true;
+        while (hasSwap) {
+            let min = this._indexOfMinKey(i); //return the index of the min element among(i, 2i, 2i+1)
+            if (min != i) {
+                this._swapItems(min, i);
+                i = min;
+            } else {
+                hasSwap = false;
+            }
+        }
+    }
+    minElem() {
+        if (this.isEmpty()) {
+            throw new Error("Invalid min() operation: Heap is empty");
+        }
+        return this._heap[1].element();
+    }
+    insertElem(elem) {
+        this._size++;
+        let newPos = new HNode(elem, this._size);
+        this._heap[this._size] = newPos;
+        this._upHeap(this._size);
+        return newPos;
+    }
+    removeMin() {
+        if (this.isEmpty()) {
+            throw new Error("Invalid minKey() operation: Heap is empty");
+        }
+        let minElem = this.minElem();
+        this._heap[1] = this._heap[this._size];
+        this._size--;
+        this._downHeap(1);
+        return minElem;
+    }
+    replaceElem(pos, elem) {
+        let i = pos._index;
+        let oldElem = pos.element();
+        pos._elem = elem;
+        this._upHeap(i);
+        this._downHeap(i);
+        return oldElem;
+    }
+    keyComparisons() {
+        return this._compCount;
+    }
+    numSwaps() {
+        return this._swapCount;
+    }
+}
+
+exports.MinHeap = MinHeap;
